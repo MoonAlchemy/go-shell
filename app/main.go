00e14e6a5b67a61e6b21f083bcfa119c19bee118
Idris <idrisferah@gmail.com>
2026-03-06 13:01:10 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,50 +8,71 @@ import (
 	"strings"
 )
 
-func main() {
+var builtins = map[string]struct{}{
+	// We add our builtin commands here
+	"echo": {},
+	"exit": {},
+	"type": {},
+}
 
+func main() {
 	reader := bufio.NewReader(os.Stdin)
-	var builtins = map[string]struct{}{
-		"echo": {},
-		"type": {},
-		"exit": {},
-	}
+
 	for {
 		fmt.Print("$ ")
 
-		command, err := reader.ReadString('\n')
+		cmd, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
 			return
 		}
-		args := strings.Fields(command)
-		if len(args) == 0 {
+
+		args := strings.Fields(cmd)
+		if len(args) < 1 {
 			continue
 		}
-		switch args[0] {
-		case "exit":
-			return
-		case "type":
 
+		switch args[0] {
+		case "echo":
+			fmt.Println(strings.Join(args[1:], " "))
+
+		case "exit":
+			os.Exit(0)
+
+		case "type":
 			if len(args) < 2 {
-				fmt.Println("you did not insert a commend to test")
+				fmt.Println(os.Stderr, "type: missing argument")
 				continue
 			}
 
 			name := args[1]
+
 			if _, ok := builtins[name]; ok {
-				fmt.Println(name, "is a shell builtin")
+				fmt.Printf("%s is a shell builtin \n", name)
+
 			} else if path, err := exec.LookPath(name); err == nil {
-				fmt.Println(name, "is", path)
+				fmt.Printf("%s is %s \n", name, path)
 			} else {
-				fmt.Println(name, "not found")
+				fmt.Printf("%s: not found\n", name)
 			}
-		case "echo":
-			fmt.Println(strings.Join(args[1:], " "))
-
 		default:
-			fmt.Printf("%s: command not found\n", args[0])
-		}
+			if err := extcmd(args); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
 
+		}
 	}
+
+}
+
+func extcmd(args []string) error {
+	path, err := exec.LookPath(args[0])
+
+	if err != nil {
+		return fmt.Errorf("%s: command not found", args[0])
+	}
+	cmd := exec.Command(path, args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
